6b203d85b97019c702e41d5ae522fc28b2dd4418
SalmaWafaa <97450111+SalmaWafaa@users.noreply.github.com>
2022-01-23 01:06:36 +0200

    Navigation bar
    Navigation bar in Search order file

---
+++ container.php
@@ -0,0 +1,16 @@
+</head>
+<body class="">
+<?php
+  include_once "StyleHome.html";?>
+  <div class="navbar">
+     <a href="Hikerhomepage.php">Home</a></li>
+      <a href="Administrator.php">Add/Delete Admins</a>
+       <a href="messages.php">Chat</a> 
+      <a href="AdminGroups.php">Add/Edit/Delete groups</a>
+      <a href="HikersProfiles.php">Hikers Profiles</a>
+      <a href="HikersTrips.php">Hikers Trips</a>
+      <a href="OrdersSearch.php">Search Orders</a>
+
+  </div>
+	<div class="container" style="min-height:500px;">
+	
