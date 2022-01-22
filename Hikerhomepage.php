f7e9bfb60efb9238b08d1b6efc82643369960f15
SalmaWafaa <97450111+SalmaWafaa@users.noreply.github.com>
2022-01-23 00:50:32 +0200

    Update Hikerhomepage.php

--- Hikerhomepage.php
+++ Hikerhomepage.php
@@ -39,7 +39,6 @@ font-family: Arial, Helvetica, sans-serif;
 
 .dropdown {
   float: left;
-  overflow: hidden;
 }
 
 .dropdown .dropbtn {
@@ -92,21 +91,15 @@ font-family: Arial, Helvetica, sans-serif;
   color: black;
 }
 .row {  
-  display: -ms-flexbox; /* IE10 */
   display: flex;
-  -ms-flex-wrap: wrap; /* IE10 */
-  flex-wrap: wrap;
 }
 
 .side {
-  -ms-flex: 30%; /* IE10 */
   flex: 30%;
   background-color: #f1f1f1;
   padding: 20px;
 }
-/* Main column */
 .main {   
-  -ms-flex: 70%; /* IE10 */
   flex: 70%;
   background-color: white;
   padding: 20px;
@@ -129,16 +122,13 @@ body {-family: Verdana, sans-serif;}
 .mySlides {display: none;}
 img {vertical-align: middle;}
 
-/* Slideshow container */
+/* Slideshow  */
 .slideshow-container {
   max-width: 1000px;
   position: relative;
   margin: auto;
 }
 
-/* Caption text */
-
-/* Number text (1/3 etc) */
 .numbertext {
   color: black;
   font-size: 12px;
@@ -166,26 +156,17 @@ img {vertical-align: middle;}
 }
 .Calendar
 {
- 
   background-color: lightgrey;
   position: absolute;
   left:75px;
   font-size: 16px;
-  top: 1220px;
+  top: 1120px;
 }
 @-webkit-keyframes fade {
   from {opacity: .4} 
   to {opacity: 1}
 }
 
-@keyframes fade {
-  from {opacity: .4} 
-  to {opacity: 1}
-}
-/* On smaller screens, decrease text size */
-@media only screen and (max-width: 300px) {
-  .text {font-size: 11px}
-}
 </style>
          <div class="navbar">
 
@@ -225,27 +206,25 @@ $result = mysqli_query($conn, "SELECT Photo FROM user where Type='hiker' and ID=
       ?> 
       <a>Welcome <?php echo $_SESSION['fname'];?></a>
       <a href="Hikerhomepage.php">Home</a>
-    <a href="Contact_Us.php">Contact us</a>
-   <a href="messages.php">Chat</a>
-    
+      <a href="Contact_Us.php">Contact us</a>
+      <a href="users.php">Chat</a>
       <a href="rogrogshop.php">Shop With Us</a>
-       <a href="ChooseTrip.php">Choose Trip</a>
+      <a href="ChooseTrip.php">Choose Trip</a>
       <a href="TripHistory.php">Trip History</a>
       <a href="Confirmation.php">Suggestion Response</a>
       <div class="dropdown">
-       <button class="dropbtn">About Us  <i class="fa fa-caret-down"></i> </button>
-       <div class="dropdown-content">
+      <button class="dropbtn">About Us  <i class="fa fa-caret-down"></i> </button>
+    <div class="dropdown-content">
       <a href="Termsandcondition.php">Terms and Conditions</a>
       <a href="AdvertisingDisclosure.php">Advertising Disclosure</a>
     </div> 
     </div> 
        <a href="HikerEdit.php"> Edit account</a>
-          <a href="Survey.php">Survey</a>
-          <a href="signout.php">Sign Out</a>
+       <a href="Survey.php">Survey</a>
+       <a href="signout.php">Sign Out</a>
 
   </div>
-    </div>
-</li>
+  </div>
 </div>
       <?php
        } ?>
@@ -260,13 +239,12 @@ $result = mysqli_query($conn, "SELECT Photo FROM user where Type='hiker' and ID=
       ?><a>Welcome <?php echo $_SESSION['fname'];?></a>
       <a href="Hikerhomepage.php">Home</a></li>
       <a href="Administrator.php">Add/Delete Admins</a>
-       <a href="messages.php">Chat</a> 
+      <a href="users.php">Chat</a> 
       <a href="AdminGroups.php">Add/Edit/Delete groups</a>
       <a href="HikersProfiles.php">Hikers Profiles</a>
-            <a href="AdminResponse.php">Suggestions of hikers</a>
-
+      <a href="AdminResponse.php">Suggestions of hikers</a>
       <a href="OrdersSearch.php">Search Orders</a>
-       <a href="signout.php">Sign Out</a>
+      <a href="signout.php">Sign Out</a>
        </div>
 
       <?php
@@ -354,14 +332,14 @@ function showSlides() {
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
+    slides[i].style.display = "none"; //3shan swar ttla3 wara ba3d msh ta7t ba3d 
   }
   slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
+  if (slideIndex > slides.length) {slideIndex = 1}    //3shan yrga3 ybd2 mn el awl ba3d m slideshow ykhlass
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";  
+  slides[slideIndex-1].style.display = "block";  //3shan y show el images
   dots[slideIndex-1].className += " active";
   setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
